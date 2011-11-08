15bc88b459775fc3f582df70aeedee04b84db1f7
Gergely Imreh <imrehg@gmail.com>
2011-11-08 15:31:24 +0800

    ml: update parametrs for ex4 to have 100% correct prediction

--- ml/mlclass-ex4/ex4.m
+++ ml/mlclass-ex4/ex4.m
@@ -183,10 +183,10 @@ fprintf('\nTraining Neural Network... \n')
 
 %  After you have completed the assignment, change the MaxIter to a larger
 %  value to see how more training helps.
-options = optimset('MaxIter', 50);
+options = optimset('MaxIter', 400);
 
 %  You should also try different values of lambda
-lambda = 1;
+lambda = 0.1;  % lambda<0.15 and MaxIter=400 seems to be good of 100% accuracy
 
 % Create "short hand" for the cost function to be minimized
 costFunction = @(p) nnCostFunction(p, ...
